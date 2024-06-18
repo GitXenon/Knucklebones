5fde4677f7f63a18f057716b56220528e3701704
Xavier Bussiere <xavier.bussiere@gmail.com>
2024-06-18 11:08:21 +0200

    Improved code readability

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ class Board:
                 else:
                     value_counts[value] = 1
             for value, count in value_counts.items():
-                if value != None:
+                if value is not None:
                     column_score[i] += int(value) * count * count
         return column_score
 
@@ -47,14 +47,17 @@ class Board:
         for _ in range(3):
             table.add_column()
 
-        if reverse:
-            for i in range(2, -1, -1):
-                table.add_row(self.grid[0][i], self.grid[1][i], self.grid[2][i])
-        else:
-            for i in range(3):
-                table.add_row(self.grid[0][i], self.grid[1][i], self.grid[2][i])
+        range_values = range(2, -1, -1) if reverse else range(3)
+        for i in range_values:
+            table.add_row(self.grid[0][i], self.grid[1][i], self.grid[2][i])
 
-        console.print(table)
+        if reverse:
+            console.print(table)
+            self.print_column_score(color=color)
+        else:
+            self.print_column_score(color=color)
+            console.print(table)
+            
 
     def print_column_score(self, color="white"):
         console.print(self.column_score(), style=color)
@@ -96,19 +99,14 @@ class Knucklebone:
         self.current_turn_index = 1 - self.current_turn_index
 
     def display_end_game_screen(self):
-        player_1_score = self.players[0].board.score()
-        player_2_score = self.players[1].board.score()
-        winner = self.players[0] if player_1_score > player_2_score else self.players[1]
+        scores = [player.board.score() for player in self.players]
+        winner = self.players[0] if scores[0] > scores[1] else self.players[1]
 
         table = Table()
         table.add_column("Player", no_wrap=True)
         table.add_column("Score", style="bold magenta", no_wrap=True)
-        table.add_row(
-            self.players[0].color, str(player_1_score), style=self.players[0].color
-        )
-        table.add_row(
-            self.players[1].color, str(player_2_score), style=self.players[1].color
-        )
+        for player in self.players:
+            table.add_row(player.color, str(player.board.score()), style=player.color)
 
         trophy = Text(f"🏆 {winner.color} Wins!", justify="center", style="bold green")
         console.print(Panel(table, title="Game Results", expand=False))
@@ -132,6 +130,10 @@ class Knucklebone:
                 ]
                 opponent_board.grid[column_idx].append(None)
 
+    def show_board(self):
+        for i in range(1, -1, -1):
+            self.players[i].board.print(reverse=i, color=self.players[i].color)
+
     def reset_game(self):
         for player in self.players:
             player.board.reset()
@@ -144,10 +146,7 @@ class Knucklebone:
                 f"[{current_player.color}]It's {current_player.color} turn! Your score is {current_player.board.score()}.\n[/{current_player.color}]You rolled a [bold]{dice_roll}[/bold]."
             )
 
-            self.players[1].board.print(reverse=1, color=self.players[1].color)
-            self.players[1].board.print_column_score(color=self.players[1].color)
-            self.players[0].board.print_column_score(color=self.players[0].color)
-            self.players[0].board.print(color=self.players[0].color)
+            self.show_board()
 
             while True:
                 choice = Prompt.ask("Please select an option", choices=["1", "2", "3"])
@@ -158,6 +157,8 @@ class Knucklebone:
                     console.print("[red]Invalid play[/red]")
             self.clear_opponent_column(choice, dice_roll)
             if self.is_game_over():
+                console.clear()
+                self.show_board()
                 self.display_end_game_screen()
                 input("Press enter to return to the main menu...")
                 self.reset_game()
